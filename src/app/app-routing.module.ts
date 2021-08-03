b05d8d17844cccb75de857112a21d4f0f9dac905
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-08-03 19:20:52 +0200

    FEAT Tags Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./@pages/home/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: 'tags',
+    loadChildren: () =>
+      import('./@pages/tags/tags.module').then((m) => m.TagsModule),
+  },
   { path: ``, redirectTo: `home`, pathMatch: `full` },
 ];
 
