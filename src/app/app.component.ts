35d270b85f98e2b57963a7567684669a28254534
Ntres <84086409+Ntres@users.noreply.github.com>
2021-08-03 20:33:56 +0200

    Navbar styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', './../styles/navbar.scss'],
 })
 export class AppComponent {
   title = 'wavecode-admin';
